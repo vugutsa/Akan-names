832a709d1c14c55fcd6f3bcfe9d38a212905e240
vugutsa <vugutsamercy84@gmail.com>
2020-10-04 00:07:20 +0300

    added variables on the js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,28 @@
-var century, year, month, dayOfTheWeek, day
+let akanFemale=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+let akanMale=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+function akanNames() {
+   century=parseInt(document.getElementById("century").value);
+   year=parseInt(document.getElementById("year").value);
+   month=parseInt(document.getElementById("month").value);
+   dayofthemonth=parseInt(document.getElementById("day").value);
+
+d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+
+CC=century
+YY=year
+MM=month
+DD=day
+
+if (CC < 1 || CC > 100){
+   alert("invalid cc");
+}
+else if (YY < 1 || YY > 100){
+   alert("invalid yy");
+}
+else if (MM ===< 1 || MM > 12){
+   alert("invalid mm");
+}
+else if (DD ===< 1 || DD > 31) 
+   alert("invalid dd");
+}
